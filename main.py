6f3467ea0214cbdc02e60ffb6a0b54e39bbefb95
lucaslingle <lucasdaxlingle@gmail.com>
2026-06-02 21:13:21 -0700

    work in progress

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
+from collections import namedtuple
 import numpy as np
 import pandas as pd
 from scipy.special import gammaln, digamma
+from collections import namedtuple
 
 parser = argparse.ArgumentParser("Simplified Kurihara VDP for DP-MoG")
 parser.add_argument("--data_csv", type=str)
@@ -11,10 +13,14 @@ parser.add_argument("--cluster_location_posterior_stddev", type=float, default=0
 parser.add_argument("--inferred_clusters_limit", type=int, default=10)  # T, everything after is tied to priors
 args = parser.parse_args()
 
-def get_pv_params():
-    return 1.0, 1.0
+BetaDistribution = namedtuple("BetaDistribution", ["alpha", "beta"])
+GaussianDistribution = namedtuple("GaussianDistribution", ["mean", "stddev"])
+InfCategoricalDistribution = namedtuple("InfCategoricalDistribution", ["headprobs", "tailsum"])
 
-def get_qv_params_init():
+def get_pv():
+    return BetaDistribution(alpha=1.0, beta=1.0)
+
+def get_qv_initial():
     # sample a hyperprior on mean: alpha / (alpha + beta)
     mean = np.random.beta(1.1, 1.1, size=[args.inferred_clusters_limit]) # [T]
     # sample a hyperprior on concentration: (alpha + beta)
@@ -22,51 +28,54 @@ def get_qv_params_init():
     # convert to (alpha, beta) variational params
     phi_v1 = mean * conc
     phi_v2 = conc - phi_v1
-    return phi_v1, phi_v2
+    return BetaDistribution(alpha=phi_v1, beta=phi_v2)
 
-def get_peta_params():
-    return 0.0, args.cluster_location_prior_stddev
+def get_peta():
+    return GaussianDistribution(mean=0.0, stddev=args.cluster_location_prior_stddev)
 
-def get_qeta_parms_init(data_dim):
-    phi_eta1 = np.random.normal(
+def get_qeta_initial(data_dim):
+    mu = np.random.normal(
         loc=0.0, 
         scale=args.cluster_location_prior_stddev, 
         size=[args.inferred_clusters_limit, data_dim],
     )
-    return phi_eta1
+    return GaussianDistribution(mean=mu, stddev=args.cluster_location_posterior_stddev)
+
+def get_px_given_eta(eta):
+    return GaussianDistribution(mean=eta, stddev=args.cluster_observation_stddev)
 
 def get_sni_info(
     xs, # [N, D]
-    qv_phi_1, # [T]
-    qv_phi_2, # [T]
-    qeta_phi_1, # [T, D]
-    pv_alpha_1, # []
-    pv_alpha_2, # []
+    qv, # ([T], [T])
+    qeta, # ([T, D], [])
+    pv, # ([], [])
 ):
     sigma_x = args.cluster_observation_stddev
-    line_11 = digamma(qv_phi_1) - digamma(qv_phi_1 + qv_phi_2)  # [T]
-    line_12 = digamma(qv_phi_2) - digamma(qv_phi_1 + qv_phi_2)  # [T]
-    line_13 = np.einsum('ld,nd->nl', qeta_phi_1 / (sigma_x ** 2), xs) + \
-        -0.5 * np.einsum('ld,ld->l', qeta_phi_1 / (sigma_x ** 2), qeta_phi_1)[None, ...] # [N, T]
+    line_11 = digamma(qv.alpha) - digamma(qv.alpha + qv.beta)  # [T]
+    line_12 = digamma(qv.beta) - digamma(qv.alpha + qv.beta)  # [T]
+    line_13 = np.einsum('ld,nd->nl', qeta.mean / (sigma_x ** 2), xs) + \
+        -0.5 * np.einsum('ld,ld->l', qeta.mean / (sigma_x ** 2), qeta.mean)[None, ...] # [N, T]
     
-    S_n_i = line_11[..., None] + \
-        np.cumsum(np.concatenate([np.array([0]), line_12[:-1]], axis=0), axis=0)[None, ...] + \
+    S_n_i = (
+        line_11[..., None] + 
+        np.cumsum(np.concatenate([np.array([0]), line_12[:-1]], axis=0), axis=0)[None, ...] + 
         line_13  # [N, T]
+    )
     exp_S_n_i = np.exp(S_n_i)  # [N, T]
     exp_S_n_headsum = np.sum(exp_S_n_i, axis=-1)  # [N]
 
-    line_11_tp1 = digamma(pv_alpha_1) - digamma(pv_alpha_1 + pv_alpha_2)  # []
-    line_12_tp1 = digamma(pv_alpha_2) - digamma(pv_alpha_1 + pv_alpha_2)  # []
-    line_13_tp1 = 0.0  # zero because qeta_phi_1 for T+1 is set to prior's mean of zero
+    line_11_tp1 = digamma(pv.alpha) - digamma(pv.alpha + pv.beta)  # []
+    line_12_tp1 = digamma(pv.beta) - digamma(pv.alpha + pv.beta)  # []
+    line_13_tp1 = # redo this it isnt zero obv
     S_n_tp1 = line_11_tp1 + np.sum(line_12) + line_13_tp1  # []
     exp_S_n_tailsum = S_n_tp1 / (1 - np.exp(line_12_tp1))  # []
 
     return exp_S_n_i, exp_S_n_headsum, exp_S_n_tailsum
-    
+
 def update_qz(exp_S_n_i, exp_S_n_sum):
     q_zi_head = exp_S_n_i / exp_S_n_sum[..., None]  # [N, T]
-    q_zi_tailsum = 1 - np.sum(q_zi_tailsum, axis=-1)
-    return q_zi_head
+    q_zi_tailsum = 1 - np.sum(q_zi_head, axis=-1)
+    return InfCategoricalDistribution(headprobs=q_zi_head, tailsum=q_zi_tailsum)
 
 def update_qv(
     q_zi_head, # [N, T]
@@ -74,9 +83,9 @@ def update_qv(
     pv_alpha_1, # []
     pv_alpha_2, #[]
 ):
+    # compute line 14 left
     qv_phi_1_new = pv_alpha_1 + np.sum(q_zi_head, axis=0) # [T]
-
-    # compute sum_j={i+1}^infty = sum_j={i+1}^T + sum_j={T+1}^infty
+    # now compute sum_j={i+1}^infty = sum_j={i+1}^T + sum_j={T+1}^infty
     # i=1 -> sum i=2 ... i=T
     # ...
     # i=T-2 -> sum i=T-1 ... i=T
@@ -89,32 +98,56 @@ def update_qv(
     cumulative = np.cumsum(pad, axis=-1)
     unflip = cumulative[:, ::-1]  # [N, T]
     qz_ip1_tailsum = unflip + q_zi_tailsum[..., None]  # [N, T]
-
+    # compute line 14 right
     qv_phi_2_new = pv_alpha_2 + np.sum(qz_ip1_tailsum, axis=0)  # [T]
-    
-    return qv_phi_1_new, qv_phi_2_new
+    return BetaDistribution(alpha=qv_phi_1_new, beta=qv_phi_2_new)
 
-def update_qeta(xs, q_zi_head, peta_lambda_1):
-    qeta_phi_1_new = peta_lambda_1 + np.einsum('nt,nd->td', q_zi_head, xs)  # [T, D]
-    qeta_phi_2_new = args.cluster_location_posterior_stddev                 # []
-    return qeta_phi_1_new, qeta_phi_2_new
+def update_qeta(xs, qz, peta):
+    return GaussianDistribution(
+        mean=peta.mean + np.einsum('nt,nd->td', qz.headprobs, xs),  # [T, D], 
+        stddev=args.cluster_location_posterior_stddev,
+    )
 
 def get_total_beta_kl_diverence(
-    qv_phi_1, # [T]
-    qv_phi_2, # [T]
-    pv_alpha_1, # []
-    pv_alpha_2, # []
+    qv, # ([T], [T])
+    pv, # ([], [])
 ):
-    log_beta_q = gammaln(qv_phi_1 + qv_phi_2) - gammaln(qv_phi_1) - gammaln(qv_phi_2)
-    log_beta_p = gammaln(pv_alpha_1 + pv_alpha_2) - gammaln(pv_alpha_1) - gammaln(pv_alpha_2)
+    log_beta_q = gammaln(qv.alpha + qv.beta) - gammaln(qv.alpha) - gammaln(qv.beta)
+    log_beta_p = gammaln(pv.alpha + pv.beta) - gammaln(pv.alpha) - gammaln(pv.beta)
     term_normalization = log_beta_q - log_beta_p
 
     term_expectation = (
-        (qv_phi_1 - pv_alpha_1) * (digamma(qv_phi_1) - digamma(qv_phi_1 + qv_phi_2)) +
-        (qv_phi_2 - pv_alpha_2) * (digamma(qv_phi_2) - digamma(qv_phi_1 + qv_phi_2))
+        (qv.alpha - pv.alpha) * (digamma(qv.alpha) - digamma(qv.alpha + qv.beta)) +
+        (qv.beta - pv.beta) * (digamma(qv.beta) - digamma(qv.alpha + qv.beta))
     )
 
     return np.sum(term_normalization + term_expectation, axis=0)
+
+def get_total_gaussian_kl_divergence(
+    qeta, # ([T, D], [])
+    peta, # ([], [])
+):
+    qeta_mu = qeta.mean
+    qeta_sigma = np.full_like(qeta_mu, fill_value=qeta.stddev)
+    peta_mu = np.full_like(qeta_mu, fill_value=peta.mean)
+    peta_sigma = np.full_like(qeta_mu, fill_value=peta.stddev)
+
+    term1 = np.log(peta_sigma / qeta_sigma)
+    term2 = (qeta_sigma * qeta_sigma) / (2.0 * peta_sigma * peta_sigma)
+    term3 = ((qeta_mu - peta_mu) * (qeta_mu - peta_mu)) / (2.0 * peta_sigma * peta_sigma)
+    term4 = np.full_like(qeta_mu, fill_value=-0.5)
+    kls = np.sum(term1 + term2 + term3 + term4, axis=-1)  # [T]
+    return np.sum(kls, axis=0) # []
+
+def get_elbo(xs, qv, qeta, pv, peta):
+    # compute the elbo assuming 
+    total_kl_beta = get_total_beta_kl_diverence(qv, pv)
+    total_kl_gauss = get_total_gaussian_kl_divergence(qeta, peta)
+    _, Sni_headsum, Sni_tailsum = get_sni_info(xs, qv, qeta, pv)
+    Sni_infsum = Sni_headsum + Sni_tailsum
+    free_energy = total_kl_beta + total_kl_gauss + 
+
+
 
 def main():
     args = parser.parse_args()
